c5fed71d4eafa78a14952a4d89489c1a55f75089
Akshat Asthana <akshatsagittarius96@gmail.com>
2020-12-29 00:53:25 +0530

    file read

--- Read_File/read_file.h
+++ Read_File/read_file.h
@@ -1,4 +1,6 @@
 //declaring getLines function here
 #include <vector>
+#include <string>
+
 //don't forget to use the appropriate namespaces
 std::vector <std::string> getLines(std::string file_path); 
