bffabd993adedc25b5275884914927ba88a04dc7
Akshat Asthana <akshatsagittarius96@gmail.com>
2021-02-04 20:54:41 +0530

    string_hashing

---
+++ CP-Algorithms/string_hashing.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <iostream>
+long long compute_hash(std::string s){
+    const int m = 1e9 + 9;
+    const int p = 31;
+    long long hash = 0;
+    int p_pow = 1;
+    for (char c: s){
+        hash = (hash + (c - 'a' + 1)*p_pow) % m;
+        p_pow = (p_pow*p) % m;
+    }
+    return hash;
+}
+
+int main(){
+    std::string s;
+    std::cout << "Enter a string: \n";
+    std::cin >> s;
+    std::cout << "Hash of " << s << " is: " << compute_hash(s);
+}
